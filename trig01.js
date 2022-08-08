00ed13dde5a1438abead099642b5d1d47c10f378
Gagan <g.deepsingh1@gmail.com>
2022-08-08 14:12:48 -0600

    trignometry lesson setup changes

---
+++ trig01.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const width = (canvas.width = window.innerWidth);
+  const height = (canvas.height = window.innerHeight);
+};
